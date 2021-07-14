7e24bfe1841a12f6ba24da954a2b850d8e4925c1
Hamid <hamid.dev@yahoo.com>
2021-07-14 16:39:17 +0430

    Update Response.php

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -17,4 +17,16 @@ class Response
             ]
         ));
     }
+
+    public static function table_not_exists()
+    {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+        header('Content-Type: application/json');
+        exit(json_encode(
+            [
+                'status' => false,
+                "message" => "Your database connection is exists but the tables are not exists, please call this below link"
+            ]
+        ));
+    }
 }
